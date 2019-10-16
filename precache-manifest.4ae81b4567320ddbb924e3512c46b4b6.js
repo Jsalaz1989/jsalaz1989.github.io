145a3c7fcd6df97f9d2970982d4052a2c60255eb
Jaime Salazar <jsalaz1989@gmail.com>
2019-10-17 00:38:34 +0200

    Updates

---
+++ precache-manifest.4ae81b4567320ddbb924e3512c46b4b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "365e8febc28394fa03d76b858c0da734",
+    "url": "/Jsalaz1989/N2TClient/index.html"
+  },
+  {
+    "revision": "fd0caa8f14cd90096bb3",
+    "url": "/Jsalaz1989/N2TClient/static/css/main.23a73ea3.chunk.css"
+  },
+  {
+    "revision": "add7f71d536fb663889e",
+    "url": "/Jsalaz1989/N2TClient/static/js/2.b3eec9fb.chunk.js"
+  },
+  {
+    "revision": "fd0caa8f14cd90096bb3",
+    "url": "/Jsalaz1989/N2TClient/static/js/main.506f28bf.chunk.js"
+  },
+  {
+    "revision": "d704e121e0641de6c87f",
+    "url": "/Jsalaz1989/N2TClient/static/js/runtime~main.ceb8e93d.js"
+  }
+]);
