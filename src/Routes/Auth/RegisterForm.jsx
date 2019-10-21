7c40937407bc1de6b5f81d3871e63cd77f272e2e
Jaime Salazar <jsalaz1989@gmail.com>
2019-10-21 19:15:21 +0200

    Added two buttons to RegisterForm

--- src/Routes/Auth/RegisterForm.jsx
+++ src/Routes/Auth/RegisterForm.jsx
@@ -5,7 +5,11 @@ import { fetchPost } from '../../helpers/fetch'
 
 const RegisterForm = ({ history }) => {
 
+    const emailAvailable = window.location.search.replace('?email=','')
     const [emailSent, setEmailSent] = useState('')
+
+    const [helpButtonEmail, setHelpButtonEmail] = useState({ icon: 'email', helpText: 'Use your email as your username', nextPage: null })
+    const [helpButtonPassword, setHelpButtonPassword] = useState({ icon: 'password', helpText: 'Password should be 6-8 characters', nextPage: null })
     
     function registerUser(values, actions) {
 
@@ -31,15 +35,16 @@ const RegisterForm = ({ history }) => {
         <BaseAuthForm 
             history={history}
             done={emailSent}
-            titleBefore='Register'
-            textBefore='Enter an email and password'
-            titleAfter='Account registered'
+            titleBefore={emailAvailable ? 'Reactivate' : 'Register'}
+            textBefore={emailAvailable ? 'Reenter your password' : 'Enter an email and password'}
+            titleAfter={emailAvailable ? 'Email resent' : 'Account registered'}
             textAfter={'An email has been sent to ' + emailSent + ' with an activation link'}
             userMustExist={false}
             passwordCheck={true}
             onSubmit={registerUser}
-            submitButtonText='Register'
-            alternateButton={{ icon: 'password', helpText: 'Password should be 6-8 characters', nextPage: null }}
+            submitButtonText={emailAvailable ? 'Resend activation email' : 'Register'}
+            helpButtonEmail={helpButtonEmail}
+            helpButtonPassword={helpButtonPassword}
         />
     )
 }
